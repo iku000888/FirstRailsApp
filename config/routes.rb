df47fbb40549a4faf9d9b7ca5cb616b1957caece
iku000888 <kanumaiku@gmail.com>
2015-03-19 00:20:45 +0900

    added a resource

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  resources :articles   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
