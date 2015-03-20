d3044f6ed7c31a65e383735c4ff9af6db206ca23
iku000888 <kanumaiku@gmail.com>
2015-03-21 00:11:00 +0900

    add strong method for security?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,7 @@ class ArticlesController < ApplicationController
       @article.save
       redirect_to @article
    end
+   def article_params
+     params.require(:article).permit(:title, :text)
+   end
 end
