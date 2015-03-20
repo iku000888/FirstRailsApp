ec63b4dffb740806af5c6f7de9d9bcdf9c3a1d13
iku000888 <kanumaiku@gmail.com>
2015-03-21 00:18:09 +0900

    add a show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,21 @@
 class ArticlesController < ApplicationController
+   
    def new
    end 
+   
    def create
       #render text: params[:article].inspect
       @article=Article.new(params[:article])
       @article.save
       redirect_to @article
    end
-   def article_params
-     params.require(:article).permit(:title, :text)
+   
+   def show
+      @article = Article.find(params[:id])
    end
+   
+   private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
 end
