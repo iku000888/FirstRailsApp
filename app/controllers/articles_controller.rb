3cb396307729ea823baa9707d06454cf46a69873
iku000888 <kanumaiku@gmail.com>
2015-03-21 00:09:04 +0900

    add content to create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
    def new
    end 
    def create
-      render text: params[:article].inspect
+      #render text: params[:article].inspect
+      @article=Article.new(params[:article])
+      @article.save
+      redirect_to @article
    end
 end
